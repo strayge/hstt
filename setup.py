d9238343d68b3e327981db83c578c41bab9bb812
strayge <strayge@gmail.com>
2021-08-22 16:20:35 +0300

    v0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='hstt',
-    version='0.0.1',
+    version='0.0.2',
     description='HTTP stress testing tool',
     long_description=readme,
     author='strayge',
